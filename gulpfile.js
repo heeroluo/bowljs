dab9ee814a73084afe01e231222016caeff6d7f1
Heero.Law <heeroluo@gmail.com>
2015-12-08 12:14:17 +0800

    提交项目构建文件

---
+++ gulpfile.js
@@ -0,0 +1,15 @@
+var gulp = require('gulp'),
+	gulpReplace = require('gulp-replace'),
+	gulpUglify = require('gulp-uglify'),
+	gulpRename = require('gulp-rename');
+
+
+gulp.task('default', function() {
+	gulp.src('./bowl-debug.js')
+		.pipe( gulpReplace(/^(\s*var\s+isDebug\s*=\s*)(?:true|false)/m, '$1false') )
+		.pipe( gulpUglify({
+			preserveComments: 'license'
+		}) )
+		.pipe( gulpRename('bowl.js') )
+		.pipe( gulp.dest('./') );
+});
